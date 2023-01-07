42ac673365925df0929d581bb36a5f06c581eaa3
gourang lawane <117590608+Gourang-pro@users.noreply.github.com>
2023-01-07 21:24:07 +0530

    Add files via upload

---
+++ addition of  matrix.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a[3][3],b[3][3],c[3][3],i,j;
+
+    printf("\nenter the first matrix : \n");
+    for ( i = 0; i<3; i++)
+    {   
+        for(j=0; j<3; j++)
+        {      
+          printf("enter the value %d %d : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("enter the second matrix : \n");
+
+    for ( i = 0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("enter the value %d %d : ",i,j);
+            scanf("%d",&b[i][j]);
+        }
+    }
+    
+    printf("the final matrix is : \n");
+
+    for ( i = 0; i < 3; i++)
+    {   
+        for(j=0; j< 3; j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+
+    for( i = 0; i < 3; i++)
+    {   
+        for(j=0; j< 3; j++)
+        {
+          printf("%d \t",c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
